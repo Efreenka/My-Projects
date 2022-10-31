9b0b2ceb1da60c74fac609f83ac391a6adcf9108
Efreenka <kaja.bilkova@seznam.cz>
2022-10-31 11:18:04 +0100

    snake game

---
+++ snake_game/main.py
@@ -0,0 +1,165 @@
+# https://www.youtube.com/watch?v=v_GzR6VPmmI&list=PLQ8x_VWW6AkuPXuUJ-lGH2aoL4vuoQg62&index=33
+from turtle import Turtle, Screen
+import time
+import random
+
+# Proměnné
+score = 0
+highest_score = 0
+
+screen = Screen()
+screen.bgcolor("green")
+screen.title("Vítejte ve SNAKE GAME")
+screen.setup(width=600, height=600)
+screen.tracer(False)
+
+# Hadí hlava, jablko & scóre
+head = Turtle("square")
+head.color("black")
+head.speed(0)
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+apple = Turtle("circle")
+apple.color("red")
+apple.penup()
+apple.goto(100, 100)
+
+score_sign = Turtle("square")
+score_sign.speed(0)
+score_sign.color("white")
+score_sign.penup()
+score_sign.hideturtle()
+score_sign.goto(0, 265)
+score_sign.write("Skóre: 0  Nejvyšší skóre: 0", align="center", font=("Arial", 18))
+                                                                        
+body_parts = []
+
+
+# Funkce
+def move():
+    if head.direction == "up":
+        y = head.ycor()                                                 # načte se současná pozice a uloží se do y
+        head.sety(y + 20)                                               # aby to šlo o 20 nahoru, nastav y + 20
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+
+def move_up(): 
+    if head.direction != "down":
+        head.direction = "up"
+
+
+def move_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+
+def move_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+def move_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+# Kliknutí na klávesy
+screen.listen()
+screen.onkeypress(move_up, "w")
+screen.onkeypress(move_down, "s")
+screen.onkeypress(move_right, "d")
+screen.onkeypress(move_left, "a")
+
+# Hlavní cyklus
+while True:
+    screen.update()
+
+    # Kontrola kolize s hranou obrazovky
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(2)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        # Skryjeme části těla při názazu
+        for one_body_part in body_parts:
+            one_body_part.goto(1500, 1500)
+
+        # Vyprázdníme list s částmi těla (šedé čtverečky)
+        body_parts.clear()
+
+        # Resetování skóre
+        score = 0
+
+        score_sign.clear()
+        score_sign.write(f"Skóre: {score}  Nejvyšší skóre: {highest_score}", align="center", font=("Arial", 18))
+
+    # Kolize hlavy s jablkem - had sní jídlo
+    if head.distance(apple) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        apple.goto(x, y)
+
+        # Přidání části těla
+        new_body_part = Turtle("square")
+        new_body_part.speed(0)
+        new_body_part.color("grey")
+        new_body_part.penup()
+        body_parts.append(new_body_part)
+
+        # Zvýšení skóre
+        score += 10
+
+        if score > highest_score:                                       
+            highest_score = score
+
+        score_sign.clear()
+        score_sign.write(f"Skóre: {score}  Nejvyšší skóre: {highest_score}", align="center", font=("Arial", 18))
+
+    for i in range(len(body_parts)-1, 0, -1):
+        x = body_parts[i - 1].xcor()
+        y = body_parts[i - 1].ycor()
+        body_parts[i].goto(x, y)
+
+    if len(body_parts) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        body_parts[0].goto(x, y)
+
+    move()
+
+    # Hlava narazila do těla
+    for one_body_part in body_parts:
+        if one_body_part.distance(head) < 20:
+            time.sleep(2)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # Skryjeme části těla při názazu
+            for one_body_part in body_parts:
+                one_body_part.goto(1500, 1500)
+
+            # Vyprázdníme list s částmi těla (šedé čtverečky)
+            body_parts.clear()
+
+            # Resetování skóre
+            score = 0
+
+            score_sign.clear()
+            score_sign.write(f"Skóre: {score}  Nejvyšší skóre: {highest_score}", align="center", font=("Arial", 18))
+
+    time.sleep(0.1)
+
+screen.exitonclick()
